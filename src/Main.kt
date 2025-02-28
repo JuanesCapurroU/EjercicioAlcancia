5c19b2bc8edb87994288bd1b1dec4143d8a7e31c
JuanesCapurroU <juan.capurro@uniminuto.edu.co>
2025-02-28 11:06:05 -0500

    Ejercicio Edificio

--- src/Main.kt
+++ src/Main.kt
@@ -1,42 +1,60 @@
 import kotlin.math.min
 
 fun main() {
-    var contadorDado1 = 0;
-    var contadorDado2=0;
+    var codigo=0;
+    var contador = 3;
+    do {
+        println("Ingrese el Codigo de acceso: ")
+        codigo = readln().toInt()
 
-    println("DADO 1: ")
-                for (i in 1..10){
-                    var min = 1;
-                    var max =  6;
-                    var Dado1 = (min..max).random()
-                    contadorDado1+=Dado1
-                    print("\t$Dado1")
+        if (codigo>=1000&& codigo<=1999){
+            println("""
+             Acceso Concedido
+             Bienvenido Empleado No. : "$codigo
+        """.trimIndent())
+            codigo = 3;
+        }else if (codigo>=2000&&codigo<=2999){
+            println("""
+            Bienvenido VISITANTE
+            
+            INGRESE SU NOMBRE: 
+        """.trimIndent())
+            var nombre = readln()
+            println("""
+            $nombre  Ingrese el motivo de su visita en el Edificio: 
+        """.trimIndent())
+            var motivo = readln()
+            println("""
+            $nombre Acceso concedido en el Edificio por el motivo de: $motivo
+        """.trimIndent())
+            codigo = 3;
+        }else {
+            println(
+                """
+                Acceso Denegado
+                Codigo Invalido..
+                Le quedan $contador intentos para ingresar codigo valido
+                De lo contrario se bloqueara el acceso..
+            """.trimMargin())
+            contador -= 1;
+            var codigoAdmin=123;
+            while (contador==-1){
+                if (contador==-1){
+                    println("SISTEMA BLOQUEADO, INGRESE EL CODIGO DE ADMIN PARA DESBLOQUEAR: ")
+                    codigo =  readln().toInt();
+                    if (codigo==codigoAdmin){
+                        contador=3
+                        println("SISTEMA DESBLOQUEADO POR EL ADMIN...")
+                    }
                 }
-    println("\nDADO 2: ")
-    for (i in 1..10){
-        var min = 1;
-        var max =  6;
-        var Dado2 = (min..max).random()
-        contadorDado2+=Dado2
-        print("\t$Dado2")
-    }
-
-    if (contadorDado1>contadorDado2){
-        println("\n")
-        println(""" 
-            El dado numero 1 saco $contadorDado1
-            El dado numero 2 saco $contadorDado2
-            Por tal motivo el ganador es el Dado numero 1
-        """.trimIndent())
-    }else {
-        println("""
-            El dado numero 1 saco $contadorDado1
-            El dado numero 2 saco $contadorDado2
-            Por tal motivo el ganador es el Dado numero 2
-        """.trimIndent())
-    }
-
             }
+        }
+    }while (contador!=-2)
+}
+
+
+
+
 
 
 
